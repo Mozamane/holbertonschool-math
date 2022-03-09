b63660f25ccbc58ddafbde6718e316361aaf5859
Mozamane <mozamanebaloyi1994@gmail.com>
2022-03-09 11:54:46 +0200

    a function that performs the multiplication operation to complex numbers.

---
+++ 0x00-math_complex/6-multiplication.c
@@ -0,0 +1,22 @@
+/*
+* 6-multiplication.c - multiplication of complex
+* Author: Mozamane Baloyi
+* Date: March 09, 2022
+*/
+
+#include "main.h"
+#include <math.h>
+
+/**
+* multiplication - return the multiplication
+* @c1: complex
+* @c2: complex
+* @c3: complex pointer
+* Return: void
+*/
+
+void multiplication(complex c1, complex c2, complex *c3)
+{
+	c3->re = (c1.re * c2.re) - (c1.im * c2.im);
+	c3->im = (c1.re * c2.im) + (c1.im * c2.re);
+}
